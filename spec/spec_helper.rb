00fcd4d6f1bd7c92a84261259b60897d564b920a
abhipatil <abhipatil@cybage.com>
2026-02-06 17:53:35 +0530

    added vulnerablity to  calculator code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 require 'simplecov'
+require 'simplecov_json_formatter'
+
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 
 SimpleCov.start do
   add_filter '/spec/'
 end
-
-require_relative '../lib/calculator'
