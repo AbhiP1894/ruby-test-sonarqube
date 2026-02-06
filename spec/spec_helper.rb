78ea875219605964b8a31213f1a33ed661b0aaab
root <root@ip-172-31-54-216.ec2.internal>
2026-02-06 12:42:46 +0000

    added build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
 require 'simplecov'
 require 'simplecov_json_formatter'
 
-SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+# Disable resultset & HTML completely
+SimpleCov.formatters = [
+  SimpleCov::Formatter::JSONFormatter
+]
 
 SimpleCov.start do
   add_filter '/spec/'
 end
+
+# Load app files explicitly
+Dir[File.join(__dir__, '../lib/**/*.rb')].sort.each { |f| require f }
 require_relative '../lib/calculator'
+
